ac240ae1e95086b115b4eb8c56ad8a5b3f6f471f
hsjoihs <hs.ioling.hs@gmail.com>
2017-02-02 10:53:13 +0900

    ver0.5.5 @ 2014May01 16:45

--- autotest.bat
+++ autotest.bat
@@ -13,14 +13,14 @@ call bat\setlogname testresult
 type nul > %LOG% 
 if exist %FAIL% del %FAIL%
 
-for /F "usebackq delims=" %%i in (`type bat\C48list.txt`)  do ( call :tester ccsc  -C48 CCS      BF_c   C48test  %%i )
-for /F "usebackq delims=" %%i in (`type bat\C88list1.txt`) do ( call :tester ccsc  -C88 BF       BF_c   C88test  %%i )
-for /F "usebackq delims=" %%i in (`type bat\C88list2.txt`) do ( call :tester ccsc  -C88 BF_i     BF_c   C88test_ %%i )
-for /F "usebackq delims=" %%i in (`type bat\C78list1.txt`) do ( call :tester ccsc  -C78 ND       BF_c   C78test  %%i )
-for /F "usebackq delims=" %%i in (`type bat\C78list2.txt`) do ( call :tester ccsc  -C78 ND_rev   BF_c   C78test_ %%i )
-for /F "usebackq delims=" %%i in (`type bat\Elist.txt`)    do ( call :tester ccsc  -E   CS       PCS    C11test  %%i )
-for /F "usebackq delims=" %%i in (`type bat\R88list.txt`)  do ( call :tester ccsrc -C88 BF_c     BF_i   R88test  %%i )
-for /F "usebackq delims=" %%i in (`type bat\R87list.txt`)  do ( call :tester ccsrc -C87 BF_c     ND_rev R88test  %%i )
+for /F "usebackq eol=; delims=" %%i in (`type bat\C48list.txt`)  do ( call :tester ccsc  -C48 CCS      BF_c   C48test  %%i )
+for /F "usebackq eol=; delims=" %%i in (`type bat\C88list1.txt`) do ( call :tester ccsc  -C88 BF       BF_c   C88test  %%i )
+for /F "usebackq eol=; delims=" %%i in (`type bat\C88list2.txt`) do ( call :tester ccsc  -C88 BF_i     BF_c   C88test_ %%i )
+for /F "usebackq eol=; delims=" %%i in (`type bat\C78list1.txt`) do ( call :tester ccsc  -C78 ND       BF_c   C78test  %%i )
+for /F "usebackq eol=; delims=" %%i in (`type bat\C78list2.txt`) do ( call :tester ccsc  -C78 ND_rev   BF_c   C78test_ %%i )
+for /F "usebackq eol=; delims=" %%i in (`type bat\Elist.txt`)    do ( call :tester ccsc  -E   CS       PCS    C11test  %%i )
+for /F "usebackq eol=; delims=" %%i in (`type bat\R88list.txt`)  do ( call :tester ccsrc -C88 BF_c     BF_i   R88test  %%i )
+for /F "usebackq eol=; delims=" %%i in (`type bat\R87list.txt`)  do ( call :tester ccsrc -C87 BF_c     ND_rev R88test  %%i )
 
 
 
@@ -82,6 +82,6 @@ set to=%4
 set tmp=%5
 set name=%6
 set num=%7
-call :compile %compiler% %option% __%frm%\%name%__%frm%.txt %tmp% __%to%\%name%__%to%.txt
+call :compile %compiler%    %option%   __%frm%\%name%__%frm%.txt   %tmp%%num%    __%to%\%name%__%to%.txt 
 endlocal
 exit /b
